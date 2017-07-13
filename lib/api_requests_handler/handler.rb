5fb63e88e74c445176031453f223c4690df7f6e8
kamalhosny <kamal-aldin-hosny@hotmail.com>
2017-07-13 18:19:03 +0200

    Add data fetcher method

--- lib/api_requests_handler/handler.rb
+++ lib/api_requests_handler/handler.rb
@@ -1,5 +1,8 @@
 module ApiRequestsHandler
   class Handler
+    ## Attributes
+    attr_accessor :method, :app_id, :retry_count
+
     ## Constructor
     def initilaize(method: 'get', app_id:, retry_count: 0, options: {})
       @method = method
@@ -24,24 +27,23 @@ module ApiRequestsHandler
     ## Instance Methods
     def http_request
       validate
+      app_data = app_data_fetcher
 
-      Request.new(@method, @retry_count, @options)
+      Request.new(method, app_data['url'], retry_count)
     end
 
     def validate
       valid_methods = %w[get post put delete]
-      raise ArgumentError, "#{@method} is not a valid method" unless valid_methods.include? @method.downcase
-      raise ArgumentError, "#{@retry_count} is not a valid number (should be > 0)" if @retry_count.negative?
+      raise ArgumentError, "#{method} is not a valid method" unless valid_methods.include? method.downcase
+      raise ArgumentError, "#{retry_count} is not a valid number (should be > 0)" if retry_count.negative?
+      raise ArgumentError, "#{app_id} is not a valid application name " if ENV["#{app_id}_APP_ID"].nil?
     end
 
-    def nameless
-      case @app_id
-      when 'core'
-        app_url = ENV['CORE_BASE_URL']
-        app_token = ENV['CORE_TOKEN']
-      end
-
+    def app_data_fetcher
+      app_id = ENV["#{app_id}_APP_ID"]
+      url    = ENV["#{app_id}_URL"]
+      token  = ENV["#{app_id}_TOKEN"]
+      Hash[:app_id, app_id, :url, url, :token, token]
     end
-
   end
 end
