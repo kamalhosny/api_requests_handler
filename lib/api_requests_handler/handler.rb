cac0bb75325c386b61cbc4235458738b4fa18d13
kamalhosny <kamal-aldin-hosny@hotmail.com>
2017-07-13 11:20:17 +0200

    Edit handler class

--- lib/api_requests_handler/handler.rb
+++ lib/api_requests_handler/handler.rb
@@ -1,15 +1,47 @@
 module ApiRequestsHandler
   class Handler
-    def initilaize (options: {})
-      @method = options[:method]
-      @app_id = options[:app_id]
-      @headers = options[:headers]
-      @body = options[:body]
+    ## Constructor
+    def initilaize(method: 'get', app_id:, retry_count: 0, options: {})
+      @method = method
+      @app_id = app_id
+      @retry_count = retry_count
+      @options = options
       @response = http_request
     end
 
-    def http_request
-      @request = Request.new()
+    ## Class Methods
+    def self.to_env(name:, url:, token:)
+      info = {
+        url: url,
+        token: token
+      }
+
+      File.open('/.env', 'a') do |file|
+        file.write "mo{'#{name.upcase}':'#{info}'\n}"
+      end
     end
+
+    ## Instance Methods
+    def http_request
+      validate
+
+      Request.new(@method, @retry_count, @options)
+    end
+
+    def validate
+      valid_methods = %w[get post put delete]
+      raise ArgumentError, "#{@method} is not a valid method" unless valid_methods.include? @method.downcase
+      raise ArgumentError, "#{@retry_count} is not a valid number (should be > 0)" if @retry_count.negative?
+    end
+
+    def nameless
+      case @app_id
+      when 'core'
+        app_url = ENV['CORE_BASE_URL']
+        app_token = ENV['CORE_TOKEN']
+      end
+
+    end
+
   end
 end
--- lib/api_requests_handler/request.rb
+++ lib/api_requests_handler/request.rb
@@ -1,17 +1,6 @@
 module ApiRequestsHandler
   class Request
-    def initilaize(options: {})
-      @action = options[:action]
-      @uri = URI(url)
-      @uri.query = URI.encode_www_form(options)
-      @response = http_action
+    def initilaize(method, app_id, )
     end
-
-    # def http_action
-    #
-    # end
   end
 end
-
-
-Request.new(method: 'get', headers: headers, body: body)
